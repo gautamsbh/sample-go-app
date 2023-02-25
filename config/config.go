6624a72a93c43223854699ae4cab86b927b0ee53
gautamsbh <gautam.singh.abes@gmail.com>
2023-02-25 06:44:29 +0530

    refactor(config): reading config from environment variable

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
 type Config struct {
 	Host string
 	Port int
@@ -7,12 +13,36 @@ type Config struct {
 
 var AppConfig Config
 
+// GetEnvStr get env string
+func GetEnvStr(key, defaultVal string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultVal
+	}
+	return val
+}
+
+// GetEnvInt get env int
+func GetEnvInt(key string, defaultVal int) int {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultVal
+	}
+
+	valInt, err := strconv.Atoi(val)
+	if err != nil {
+		log.Fatalf("invalid environment value env: %s val: %s", key, val)
+	}
+
+	return valInt
+}
+
 // initialize config
 //
 // To read from environment variable, os.Getenv("ENV_NAME")
 func init() {
 	AppConfig = Config{
-		Host: "",
-		Port: 8000,
+		Host: GetEnvStr("APP_HOST", ""),
+		Port: GetEnvInt("APP_PORT", 8000),
 	}
 }
